c7315fdc86dd1ed8a1728761bdc950c3abd4dd12
croftik <Chayka_Lory@mail.ru>
2020-10-03 22:27:07 +0300

    feat: complete task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+  if (!Array.isArray(array)) return false;
+  let result = [];
+  for (let i = 0; i < array.length; i++){
+    if (typeof array[i] === 'string'){
+      result.push(array[i].trim().charAt(0).toUpperCase());
+    }
+  }
+  return result.sort().join('');
 };
